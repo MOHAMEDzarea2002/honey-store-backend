5cffed5822d46f01f5b2483dc1e8ba887d6d9a00
mohamed zarea <mohamedzarea@gmial.com>
2026-08-03 17:10:05 +0300

    update Environment Variables

--- config/firebase.js
+++ config/firebase.js
@@ -2,10 +2,13 @@ const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 const {getAuth} = require('firebase-admin/auth');
 
-const serviceAccount = require('./serviceAccountKey.json');
 
 initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  }),
 });
 
 const db = getFirestore();
