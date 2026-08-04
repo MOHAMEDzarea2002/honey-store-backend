516acea39d595f0f096e28cdc0b64e138637eb2f
mohamed zarea <mohamedzarea@gmial.com>
2026-08-04 21:27:27 +0300

    debug

--- config/firebase.js
+++ config/firebase.js
@@ -2,9 +2,7 @@ const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 const {getAuth} = require('firebase-admin/auth');
 const serviceAccountKey = require('./serviceAccountKey.json')
-console.log("PROJECT:", process.env.FIREBASE_PROJECT_ID);
-console.log("EMAIL:", process.env.FIREBASE_CLIENT_EMAIL);
-console.log("KEY EXISTS:", !!process.env.FIREBASE_PRIVATE_KEY);
+
 initializeApp({
   credential: cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
