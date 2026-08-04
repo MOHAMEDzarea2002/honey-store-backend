77bc53e4c23b9235efa01a2027df3dcf1fe980f2
mohamed zarea <mohamedzarea@gmial.com>
2026-08-05 00:24:41 +0300

    update server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-// require('./config/firebase');
+require('./config/firebase');
 
 const orderRoutes = require('./routes/orderRoutes');
 const productRoutes = require('./routes/ProductRoutes');
@@ -10,9 +10,7 @@ const dashboardRoutes = require('./routes/dashboardRoutes');
 
 const app = express();
 
-console.log('FIREBASE_PROJECT_ID:', process.env.FIREBASE_PROJECT_ID);
-console.log('FIREBASE_CLIENT_EMAIL:', process.env.FIREBASE_CLIENT_EMAIL);
-console.log('FIREBASE_PRIVATE_KEY exists:', !!process.env.FIREBASE_PRIVATE_KEY);
+
 app.use(cors());
 app.use(express.json());
 app.use('/api/orders', orderRoutes);
@@ -22,5 +20,5 @@ app.use('/api/dashboard', dashboardRoutes);
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server Running On Port ${PORT}`);
+  console.log(` Server Running On Port ${PORT}`);
 });
